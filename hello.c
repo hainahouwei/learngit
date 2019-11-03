42ec9909881d6e160d016cec486a5c505bb786ec
hainahouwei <1432957333@qq.com>
2019-11-03 20:34:43 +0800

    branch test

--- hello.c
+++ hello.c
@@ -12,6 +12,11 @@ void main()
 	{
 		i=0;
 	}
-	printf("try again!\r\n");
+	    printf("try again!\r\n");
         printf("what are you doing?");    
+        printf("i'm missing you !");
+        printf("branch test .....");
+        printf("what a fucking dog!");
+        printf("what a fucking cat !");
+		printf("branch test again!");
 }
